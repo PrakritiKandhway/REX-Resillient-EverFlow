8c3196cfa3c63ebc00729f9815ad201983fe4c44
PrakritiKandhway <prakritikandhway@gmail.com>
2026-04-22 12:49:18 +0530

    fixed merge conflict and finalized controller

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,13 +1,7 @@
 const Order = require("../models/Order");
-<<<<<<< Updated upstream:Backend/controllers/orderController.js
-const {checkRisk,suggestSupplier}=require("../services/riskServices")
-const Inventory=require("../models/Inventory");
-const Supplier=require("../models/Supplier");
-=======
 const { checkRisk, suggestSupplier } = require("../services/riskServices");
 const Supplier = require("../models/Supplier");
 const Alert = require("../models/Alert");
->>>>>>> Stashed changes:everflow-backend/controllers/orderController.js
 
 const { getWeatherRisk } = require("../services/weatherServices");
 const { calculateRisk } = require("../services/riskEngine");
@@ -19,51 +13,37 @@ exports.addOrder = async (req, res) => {
   try {
     const { productId, supplierId, quantity } = req.body;
 
-    // Data from middleware
     const supplier = req.supplier;
     const product = req.product;
     const inventory = req.inventory;
 
-    // Create Order
     const order = await Order.create({
       productId,
       supplierId,
       quantity
     });
 
-<<<<<<< Updated upstream:Backend/controllers/orderController.js
-    const risks=checkRisk(order,inventory,supplier);
-    const suggestion=suggestSupplier(allSuppliers);
-=======
-    // Get all suppliers (for suggestion)
     const allSuppliers = await Supplier.find();
 
-    // Detect risks (rule-based)
     const risks = checkRisk(order, inventory, supplier);
 
-    // Weather data
     const weatherData = await getWeatherRisk(supplier.location);
 
-    // Risk score
     const riskData = calculateRisk(supplier, weatherData.risk);
 
-    // Impact analysis (dynamic)
     const impact = calculateImpact(
       supplier.supplyVolumePerDay,
       supplier.avgDelayDays,
       product.profitPerUnit
     );
 
-    // Recommendations
     const recommendations = getRecommendations(
       riskData.riskLevel,
       supplier.avgDelayDays
     );
 
-    // Suggest supplier
     const suggestion = suggestSupplier(allSuppliers);
 
-    // Create alerts
     for (let risk of risks) {
       await Alert.create({
         type: risk.type,
@@ -73,8 +53,6 @@ exports.addOrder = async (req, res) => {
       });
     }
 
-    // Final response
->>>>>>> Stashed changes:everflow-backend/controllers/orderController.js
     res.status(201).json({
       order,
       supplier,
